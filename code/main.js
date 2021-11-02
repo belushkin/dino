78bed5fdc70e6a13f10acc05773ce875ec1c5461
Maksym Bielushkin <MukcTypa@yahoo.com>
2021-11-02 14:57:52 +0000

    Added template

--- code/main.js
+++ code/main.js
@@ -1,15 +1,34 @@
 import kaboom from "kaboom";
 
 // initialize context
-kaboom();
+kaboom({
+  width: 680,
+  height: 120,
+  background: [ 255, 255, 255, ],
+  // Explain this
+  root: document.querySelector("body > div > div.game")
+});
 
-// load assets
-loadSprite("bean", "sprites/bean.png");
+// Explain this
+// loadSpriteAtlas("sprites/dino.png", {
+//     "dino": {
+//         x: 1340,
+//         y: 3,
+//         width: 527,
+//         height: 95,
+//         sliceX: 6,
+//         anims: {
+//             idle: { from: 0, to: 1 },
+//             run: { from: 2, to: 3 },
+//             hit: { from: 4, to: 5 },
+//         },
+//     },
+// });
+loadSpriteAtlas("sprites/dino.png", "sprites/dino.json");
 
-// add a character to screen
-add([
-	// list of components
-	sprite("bean"),
-	pos(80, 40),
-	area(),
+const dino = add([
+    // pos(50, 50),
+    sprite("dino"),
 ]);
+
+dino.play("run");
