4324b549758896a0b11afec2f18beefb66463a1e
Maksym Bielushkin <MukcTypa@yahoo.com>
2021-11-10 18:34:55 +0000

    move locally

--- code/kaboom.js
+++ code/kaboom.js
@@ -3,7 +3,7 @@ import kaboom from "kaboom";
 export const k = kaboom({
   width: 1000,
   height: 220,
-  background: [255, 255, 0],
+  background: [0, 0, 0],
   // Explain this
   root: document.querySelector("body > div > div.game"),
 });
--- code/main.js
+++ code/main.js
@@ -4,8 +4,9 @@ import { realFloor } from "./src/floor";
 import key_handlers from "./src/key_handlers";
 import cloud from "./src/cloud";
 import cactus from "./src/cactus";
+import hi from "./src/hi";
 
-const DINO_POSITION_X = 0;
+const DINO_POSITION_X = 5;
 const DINO_POSITION_Y = 129;
 
 loadSpriteAtlas("sprites/dino.png", "sprites/dino.json");
@@ -28,19 +29,9 @@ const dino = add([
   sprite("dino"),
 ]);
 
-add([pos(30, 30), sprite("0")]);
-add([pos(55, 30), sprite("1")]);
-add([pos(80, 30), sprite("2")]);
-add([pos(105, 30), sprite("3")]);
-add([pos(130, 30), sprite("4")]);
-add([pos(155, 30), sprite("5")]);
-add([pos(180, 30), sprite("6")]);
-add([pos(205, 30), sprite("7")]);
-add([pos(230, 30), sprite("8")]);
-add([pos(255, 30), sprite("9")]);
-
 // Load handlers
 key_handlers(dino, start);
 cloud();
 cactus();
 realFloor();
+hi();
